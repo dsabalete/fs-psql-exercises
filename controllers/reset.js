883aec5d2ce0b287d7eba1bee07656e379ff33ec
David <dsabalete@gmail.com>
2026-04-19 19:12:59 +0200

    Exercise 20: Reading list

--- controllers/reset.js
+++ controllers/reset.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
-const { Blog, User } = require('../models')
+const { Blog, User, ReadingList } = require('../models')
 
 router.post('/', async (req, res) => {
+    await ReadingList.destroy({ where: {}, truncate: true, cascade: true })
     await Blog.destroy({ where: {}, truncate: true, cascade: true })
     await User.destroy({ where: {}, truncate: true, cascade: true })
     res.status(204).end()
--- models/index.js
+++ models/index.js
@@ -1,10 +1,21 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readinglist')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: ReadingList, as: 'readings', foreignKey: 'userId', otherKey: 'blogId' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'usersWhoRead', foreignKey: 'blogId', otherKey: 'userId' })
+
+User.hasMany(ReadingList)
+ReadingList.belongsTo(User)
+
+Blog.hasMany(ReadingList)
+ReadingList.belongsTo(Blog)
+
 module.exports = {
     Blog,
-    User
+    User,
+    ReadingList
 }
